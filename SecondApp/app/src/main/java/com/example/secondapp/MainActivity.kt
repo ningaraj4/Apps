f567296e403354b11004030b0ff59574ed46f463
ningaraj4 <ningarajtotagi4@example.com>
2024-10-28 15:15:58 +0530

    finish

--- SecondApp/app/src/main/java/com/example/secondapp/MainActivity.kt
+++ SecondApp/app/src/main/java/com/example/secondapp/MainActivity.kt
@@ -1,5 +1,4 @@
-
-package com.example.myapplication
+package com.example.secondapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.secondapp.ui.theme.SecondAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            SecondAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -41,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApplicationTheme {
+    SecondAppTheme {
         Greeting("Android")
     }
 }
